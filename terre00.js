2cbe6225722e28f97a742e56abd62f61aeca2d6e
salim <salimdu15@outlook.fr>
2023-03-04 09:49:26 +0100

    exo de lalphabet

---
+++ terre00.js
@@ -0,0 +1,8 @@
+var alphabet = "";
+
+// boucle qui utilise le code ASCI pour afficher l
+for(var i = 97; i < 97+26; i++){
+    alphabet=alphabet+String.fromCharCode(i);
+}
+console.log(alphabet);
+
