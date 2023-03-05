9d5fad6a09907c37048760202aaa4e977b182b93
salim <salimdu15@outlook.fr>
2023-03-05 03:03:05 +0100

    exo5 Division

--- terre05.js
+++ terre05.js
@@ -3,13 +3,19 @@ const args = process.argv;
 var valeurEntree01 =parseInt(args[2]);
 var valeurEntree02 = parseInt(args[3]);
 
-if((valeurEntree01 >= valeurEntree02) && (valeurEntree02 > 0)){
+if(args.length == 4)
+    if((valeurEntree01 >= valeurEntree02) && (valeurEntree02 > 0)){
 
-    var resulat = Math.trunc(valeurEntree01/valeurEntree02);
-    var reste = valeurEntree01%valeurEntree02;
-    console.log("resulat : "+resulat);
-    console.log("reste : "+reste);
-}else
-    console.log("Erreur");
-    
+        var resulat = Math.trunc(valeurEntree01/valeurEntree02);
+        var reste = valeurEntree01%valeurEntree02;
+        console.log("resulat : "+resulat);
+        console.log("reste : "+reste);
+    }else
+        console.log("Erreur");
+else 
+    if( args.length > 4)
+        console.log("Trop d'argument !");
+    else 
+        console.log("peut d'argument !");
+        
 
