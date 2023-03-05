44f2401cc4af95a7baf517da3407c3d93085507e
salim <salimdu15@outlook.fr>
2023-03-05 19:26:28 +0100

    exo11 24 to 12

--- terre11.js
+++ terre11.js
@@ -1,13 +1,37 @@
 const args = process.argv;
 
 var valeurEntree = args[2];
+var heure = "";
+var minutes = "";
+
+function heure24(valeurEntree){
+    
+    if(valeurEntree[2] === ":" && valeurEntree[4] != undefined){
+        heure = valeurEntree[0]+valeurEntree[1];
+        minutes = valeurEntree[3]+valeurEntree[4]; 
+    }else 
+        if(valeurEntree[2] === ":" && valeurEntree[4] == undefined){
+            heure = valeurEntree[0]+valeurEntree[1];
+            minutes = valeurEntree[3]+0; 
+        }else
+            if(valeurEntree[1] === ":" && valeurEntree[2] != undefined){
+                heure = 0+valeurEntree[0];
+                if(valeurEntree[3] == undefined)
+                    minutes = valeurEntree[2]+0;
+                else 
+                    minutes = valeurEntree[2]+valeurEntree[3];  
+            }else 
+                if(valeurEntree[1] === ":" && valeurEntree[3] == undefined){
+                    heure = 0+valeurEntree[1];
+                    minutes = valeurEntree[2]+0; 
+                }
+}
 
 if(args.length == 3){
-    var heure = valeurEntree[0]+valeurEntree[1];
-    var minutes = valeurEntree[3]+valeurEntree[4];
-    if(valeurEntree[2] === ":" && heure > 12 && heure < 24 && minutes < 60 && minutes > -1){
+    
+    heure24(valeurEntree);
+    if(heure > 12 && heure < 24 && minutes < 60 && minutes > -1){
         console.log(heure-12+":"+minutes+"PM");
-        
     }else
         if(heure == "00" && minutes < 60 && minutes > -1)
             console.log(parseInt(heure)+12+":"+minutes+"PM");
