3882defe3db535851a7c9d6725957263a4152ef6
salim <salimdu15@outlook.fr>
2023-03-05 03:02:30 +0100

    exo4 pair ou impair

--- terre04.js
+++ terre04.js
@@ -2,13 +2,19 @@ const args = process.argv;
 
 const valeurEntree = args[2];
 
-if(valeurEntree >= 0){
-    if(valeurEntree % 2 == 0 )
-        console.log("Paire");
+if(args.length == 3)
+    if(valeurEntree >= 0){
+        if(valeurEntree % 2 == 0 )
+            console.log("Paire");
     else 
         console.log("Impaire");
 }else
     console.log("Tu ne me la mettra pas a l'invers.");
+else 
+    if( args.length > 3)
+        console.log("Trop d'argument !");
+    else 
+        console.log("peut d'argument !");
     
 
     
